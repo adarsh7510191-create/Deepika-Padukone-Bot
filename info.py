149fa5508bf50b28f7ea2c7def1f4c2f46a5a089
adarsh7510191-create <adarsh7510191@gmail.com>
2025-11-22 14:01:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1003454917075")# Channels for
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')    # QR code image for payments
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '@Alan_Walker_2')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "7day",
