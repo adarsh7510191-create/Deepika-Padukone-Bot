dbdc6ce75ba69661c5a4b57d7876dc8820c27bfd
adarsh7510191-create <adarsh7510191@gmail.com>
2025-12-31 11:20:36 +0530

    Change AUTH_CHANNELS value for force subscription
    Updated the AUTH_CHANNELS environment variable for force subscription.

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group lin
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003185717155")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1003454917075")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1003639205036")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
