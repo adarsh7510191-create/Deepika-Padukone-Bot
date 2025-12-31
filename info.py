82d95084e5160093fa444b1e2abcbc4bf1f23542
adarsh7510191-create <adarsh7510191@gmail.com>
2025-12-31 11:18:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1003214017673'))  # Bin channel i
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1003345400833'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1003059566568').split()] #(make sure bot is admin)
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1003356559265')  # Support group id (make sure bot is admin)
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003439670727')  # Request channel id (make sure bot is admin)
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003185717155')  # Request channel id (make sure bot is admin)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
